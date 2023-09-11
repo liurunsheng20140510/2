667072733e4b47dfc2c838351254869409030d65
liurunsheng20140510 <144590095+liurunsheng20140510@users.noreply.github.com>
2023-09-11 20:58:15 +0800

    Update 5.py

--- 5.py
+++ 5.py
@@ -1,3 +1,17 @@
-li = ["hello",'seven',["mon",["h","kelly"],'all'],446]
-print(li[2][1][1])
-li[2][1] = ALL
+s = ""
+d = ""
+f = ""
+a = input("一串数字")
+for i in a:
+  if i % 2 == 0:
+    if i not in s:
+      s.append(i)
+    elif i in s:
+      i += 1
+  elif i % 2 == 1:
+    if i not in d:
+      d.append(o)
+    elif i in d:
+      d += 1
+g = len(a)
+f.append(g)
