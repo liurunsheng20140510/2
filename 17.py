3745db1becce2deac7291f498f2e2f7f901ca1e9
liurunsheng20140510 <144590095+liurunsheng20140510@users.noreply.github.com>
2023-12-14 20:02:01 +0800

    Update 17.py

--- 17.py
+++ 17.py
@@ -1,5 +1,6 @@
-a = input()
-b = []
-open(a + ".txt")
-c = b.readlines
-print(c)
+class Student():
+    def report(self,a,b,c,d):
+        report.__init__(self)
+        print(a,b,c,d,a+b+c+d)
+zhangsan = Student("张三 ",95,100,80)
+zhangsan.report()
