782a1c4360cbb2007bc48395b708638a1650558e
liurunsheng20140510 <144590095+liurunsheng20140510@users.noreply.github.com>
2023-12-19 20:44:11 +0800

    18.py

--- 18.py
+++ 18.py
@@ -2,11 +2,23 @@ class A:
     def __init__(self,name,count):
         self.count = count
         self.name = name
-    def add(self):
         a = open("self.name" + ".txt",a)
-        a.white("存入" + self.count)
-        a.close()
-    def self(self):
+        a.white("铅笔" + self.count + "创建账户")
+    def add(self,s):
+        self.count + s
         a = open("self.name" + ".txt",a)
-        a.white("卖出" + self.count)
+        a.white("铅笔" + self.count + "进货" + s)
         a.close()
+    def self(self,d):
+        self.count - d
+        a = open("self.name" + ".txt",a)
+        a.white("铅笔" + self.count + "卖出" + d)
+        a.close()
+while 1:
+    f = input("请输入你要选择的操作，1 进货，2 卖出")
+    if f == 1:
+        g = input("进货数量")
+        add(g)
+    elif f == 2:
+        h = input("卖出数量")
+        sell(h)
