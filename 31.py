a536194fd871cc09da350a51045aea003c7ee400
liurunsheng20140510 <144590095+liurunsheng20140510@users.noreply.github.com>
2024-03-05 21:22:52 +0800

    31.py

--- 31.py
+++ 31.py
@@ -1,7 +1,7 @@
 a = []
 for i in range(1000):
     a.append(0)
-for i in range(1001):
+for i in range(1,1001):
     if i % 5 == 0:
         a[i-1] += 1
     if i % 7 == 0:
@@ -10,4 +10,11 @@ for i in range(1001):
         a[i-1] += 1
     if i % 12 == 0:
         a[i-1] += 1
-print(a)
+b = max(a)
+print("最多按了{}下".format(b))
+c = min(a)
+print("最少按了{}下".format(c))
+d = a.count(0)
+e = a.count(1)
+f = a.count(2)
+print("最后亮了{}盏灯".format(d+e+f))
